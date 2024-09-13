29ad9728f9b3dca50939de136a22e87724928370
duanhongyi <duanhongyi@doopai.com>
2024-09-13 13:26:58 +0800

    chore(filer): add timeout for read and write

--- cmd/filer.go
+++ cmd/filer.go
@@ -14,8 +14,8 @@ func main() {
 	bind := flag.String("bind", ":8100", "port to serve on")
 	path := flag.String("path", ".", "the directory of file to host")
 	buffsize := flag.Int64("buffsize", 32<<16, "memory size for multipart form")
-	duration := flag.Int64("duration", 3600, "provide a pass for the account")
-	waittime := flag.Int64("waittime", 1200, "provide a pass for the account")
+	duration := flag.Int64("duration", 3600, "start duration for filer")
+	waittime := flag.Int64("waittime", 1200, "close waittime for filer")
 	username := flag.String("username", "drycc", "provide a user for the account")
 	password := flag.String("password", "drycc", "provide a pass for the account")
 
@@ -25,8 +25,8 @@ func main() {
 	server := &http.Server{
 		Addr:           *bind,
 		Handler:        api.NewFilerHandler(*username, *password, *path, *buffsize, *duration, timer),
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    time.Duration(*duration) * time.Second,
+		WriteTimeout:   time.Duration(*duration) * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 	go func() {
